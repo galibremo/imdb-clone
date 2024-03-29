93ccbe230946e54b56db9fcf37957108337523d4
galibremo <galibremo@gmail.com>
2024-03-29 15:51:58 +0600

    Add Movie Funtionality

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -15,6 +15,7 @@ export default function Card({ result }) {
           height={300}
           className="sm:rounded-t-lg group-hover:opacity-75 transition-opacity duration-300"
           alt="movie poster"
+          priority
         ></Image>
         <div className="p-2">
           <p className="line-clamp-2 text-md">{result.overview}</p>
