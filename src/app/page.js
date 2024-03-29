ed54a9186baaafdf7ef4154be715a600b7442f1d
galibremo <galibremo@gmail.com>
2024-03-29 12:48:42 +0600

    Add Loading Effect Using Next js

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,32 @@
 import Results from "@/components/Results";
 import React from "react";
+import { resolve } from "styled-jsx/css";
 
 const API_KEY = process.env.API_KEY;
 
 export default async function Home({ searchParams }) {
   const genre = searchParams.genre || "fetchTrending";
+
   const res = await fetch(
     `https://api.themoviedb.org/3${
       genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
     }?api_key=${API_KEY}&language=en-US&page=1`,
     { next: { revalidate: 10000 } }
   );
+
+  // delay 2 sec
+  // const res = await new Promise((resolve) => {
+  //   setTimeout(async () => {
+  //     const res = await fetch(
+  //       `https://api.themoviedb.org/3${
+  //         genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
+  //       }?api_key=${API_KEY}&language=en-US&page=1`,
+  //       { next: { revalidate: 10000 } }
+  //     );
+  //     resolve(res);
+  //   }, 2000);
+  // });
+
   const data = await res.json();
   if (!res.ok) {
     throw new Error("Failed to fetch data");
