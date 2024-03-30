9e2f29bd6804397a4a9808db8c5ded79707c3aa7
galibremo <galibremo@gmail.com>
2024-03-30 12:22:46 +0600

    search without a button

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,20 +1,23 @@
 "use client";
+/**********search with out a button ********* */
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 
 export default function SearchBox() {
   const [search, setSearch] = useState("");
   const router = useRouter();
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    router.push(`/search/${search}`);
-  };
+
+  useEffect(() => {
+    if (search) {
+      router.push(`/search/${search}`);
+    } else {
+      router.push("/");
+    }
+  }, [search]);
+
   return (
-    <form
-      className="flex justify-between px-5 max-w-7xl mx-auto"
-      onSubmit={handleSubmit}
-    >
+    <div className="flex justify-between px-5 max-w-7xl mx-auto">
       <input
         type="text"
         placeholder="Search keywords..."
@@ -22,12 +25,40 @@ export default function SearchBox() {
         value={search}
         onChange={(e) => setSearch(e.target.value)}
       />
-      <button
-        className="text-amber-600 disabled:text-gray-400"
-        disabled={search === ""}
-      >
-        Search
-      </button>
-    </form>
+    </div>
   );
 }
+// *********************use A button to search**********************
+// "use client";
+
+// import { useState } from "react";
+// import { useRouter } from "next/navigation";
+
+// export default function SearchBox() {
+//   const [search, setSearch] = useState("");
+//   const router = useRouter();
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     router.push(`/search/${search}`);
+//   };
+//   return (
+//     <form
+//       className="flex justify-between px-5 max-w-7xl mx-auto"
+//       onSubmit={handleSubmit}
+//     >
+//       <input
+//         type="text"
+//         placeholder="Search keywords..."
+//         className="w-full h-14 rounded-md placeholder-gray-500 outline-none bg-transparent flex-1"
+//         value={search}
+//         onChange={(e) => setSearch(e.target.value)}
+//       />
+//       <button
+//         className="text-amber-600 disabled:text-gray-400"
+//         disabled={search === ""}
+//       >
+//         Search
+//       </button>
+//     </form>
+//   );
+// }
